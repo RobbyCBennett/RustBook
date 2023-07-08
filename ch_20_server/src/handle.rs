663a2a85b5e0eeeb57f99eb4c2f9100dad84b4bb
Robby Bennett <robby@scitools.com>
2023-07-08 12:25:49 -0600

    req and res

--- ch_20_server/src/handle.rs
+++ ch_20_server/src/handle.rs
@@ -1,6 +1,7 @@
 use std::io::BufReader;
 use std::io::BufRead;
 use std::net::TcpStream;
+use std::io::prelude::*;
 
 use crate::limits::*;
 
@@ -14,30 +15,45 @@ macro_rules! debug {
 	($x:expr) => { std::convert::identity($x) }
 }
 
+pub fn respond(stream: &mut TcpStream, message: &str)
+{
+	let result = stream.write_all(message.as_bytes());
+	if result.is_err() {
+		eprintln!("{:?}", result.unwrap());
+	}
+}
+
 pub fn handle(mut stream: TcpStream)
 {
-	let buf_reader = BufReader::with_capacity(REQ_BYTES, &mut stream);
+	let buf_reader = BufReader::with_capacity(REQ_BYTES, &stream);
 
 	debug!("Request:");
 
-	let mut bytes: usize = 0;
+	let mut byte_count: usize = 0;
 	for line_result in buf_reader.lines() {
 		// Get line or error
 		if line_result.is_err() {
-			eprintln!("ERROR: Failed to read this line");
-			break;
+			eprintln!("ERROR: {:?} Failed to read this line", stream.peer_addr());
+			return respond(&mut stream, "HTTP/1.1 400 Bad Request\r\n\r\n");
 		}
 		let line = line_result.unwrap();
 
-		// Add to bytes or error if request is too large
+		// Add to byte count or error if request is too large
 		let length = line.len();
-		let add_result = bytes.checked_add(length);
+		let add_result = byte_count.checked_add(length);
 		if add_result == None || add_result.unwrap() > REQ_BYTES {
-			eprintln!("ERROR: Too many bytes");
-			break;
+			eprintln!("ERROR: {:?} Too many bytes", stream.peer_addr());
+			return respond(&mut stream, "HTTP/1.1 413 Payload Too Large\r\n\r\n");
 		}
-		bytes = add_result.unwrap();
+		byte_count = add_result.unwrap();
 
 		debug!("    {line}");
+
+		// Stop reading the request
+		if line.is_empty() {
+			break;
+		}
 	}
+
+	return respond(&mut stream, "HTTP/1.1 200 OK\r\n\r\n");
 }
